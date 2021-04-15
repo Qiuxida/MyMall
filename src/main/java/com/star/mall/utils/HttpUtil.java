7181307510ea0d3f2f9fa137d9079b7a1f618b7f
799484369@qq.com <ga34996719>
2021-04-16 00:25:02 +0800

    优化请求IP获取

---
+++ src/main/java/com/star/mall/utils/HttpUtil.java
@@ -0,0 +1,41 @@
+package com.star.mall.utils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+
+/**
+ * Http工具类
+ * @author Sam
+ * @since 2021-04-15
+ */
+public class HttpUtil {
+    /**
+     * 获取请求的ip地址
+     * @param request
+     * @return 请求的ip地址
+     */
+    public static String getIpAddress(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+            if (ip.equals("127.0.0.1") || ip.equals("")) {
+                //根据网卡取本机配置的IP
+                InetAddress inet = null;
+                try {
+                    inet = InetAddress.getLocalHost();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                ip = inet.getHostAddress();
+            }
+        }
+        return ip;
+    }
+}
